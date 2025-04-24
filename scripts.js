b5832cb6e78d8716fe9128e3b960d610f29b2871
Kiran Kanchan <155144115+Git-Kiran-uK@users.noreply.github.com.>
2025-04-24 22:18:36 +0530

    feat: Style and add functionality to the button El
    Added proper styling and added some functionality
    using js.

---
+++ scripts.js
@@ -0,0 +1,71 @@
+const containerEl = document.querySelector('.container');
+const choiceContainerEl = document.querySelector('.choice-container');
+const roundResultEl = document.querySelector('.round-result');
+const userEl = document.getElementById('user');
+const computerEl = document.getElementById('computer');
+const playBtnEl = document.getElementById('play');
+let userScore = 0;
+let computerScore = 0;
+
+document.body.addEventListener('click', (e) => {
+    if(e.target.tagName === "BUTTON"){
+        if(!playBtnEl.classList.contains('visible')) playBtnEl.classList.add('visible');
+        if(e.target.id !== playBtnEl.id){
+            e.target.focus();
+        }
+    } else {
+        playBtnEl.classList.remove('visible');
+    }
+});
+
+function handleClick(userChoice){
+    const computerChoice = getComputerChoice();
+    displayChoices(userChoice,computerChoice);
+    const userWonRound = isUserWonRound(userChoice, computerChoice);
+    handleScore(userWonRound);
+
+}
+
+function getComputerChoice(){
+    const choices = ['🪨', '📄', '✂️'];
+    const randomChoiceIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomChoiceIndex];
+}
+
+function isUserWonRound(userChoice, computerChoice){
+    switch(userChoice){
+        case '🪨':
+            if(computerChoice === '📄'){
+                return false;
+            } else if(computerChoice === '✂️'){
+                return true;
+            }
+        case '📄':
+            if(computerChoice === '✂️'){
+                return false;
+            } else if(computerChoice === '🪨'){
+                return true;
+            }
+        case '✂️':
+            if(computerChoice === '🪨'){
+                return false;
+            } else if(computerChoice === '📄'){
+                return true;
+            }
+    }
+}
+
+function handleScore(isUserWonRound){
+    if(isUserWonRound){
+        userScore++;
+    } else {
+        computerScore++;
+    }
+}
+
+function displayChoices(userChoice,computerChoice){
+    choiceContainerEl.style.display = "none";
+    userEl.textContent = userChoice;
+    computerEl.textContent = computerChoice;
+    roundResultEl.style.display = "flex";
+}
